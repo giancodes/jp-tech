e1a7beaec2d3b2753dbe943cc680976067bf7e3d
Himanshu-1433 <himanshu1433@gmail.com>
2023-08-01 23:03:55 +0530

    New Component Are Ready

---
+++ app/src/Services/service.view.php
@@ -0,0 +1,98 @@
+<?php 
+    include_once "../navbar-sec/navbar.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../navbar-sec/navbar.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./services.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <title>Services</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="heading">
+            <h1>Our Services</h1>
+            <hr>
+            <p>Welcome to our site, <b>ShopMe</b></p>
+            <p>where we take immense pride in offering you nothing but the best services. Our commitment to excellence
+                shines through in every aspect of your shopping experience. From an extensive range of high-quality
+                products to a seamless and user-friendly website, we ensure that your journey with us is nothing short
+                of exceptional. Our customer service team is dedicated to providing personalized assistance, addressing
+                any queries or concerns promptly and efficiently.</p>
+            <p>fast and reliable shipping options, your purchases will reach you in no time. We believe in building
+                lasting relationships with our valued customers, which is why we strive to go above and beyond to exceed
+                your expectations. Discover the true meaning of top-notch service and shop with confidence at our store,
+                where your satisfaction is our utmost priority.</p>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-3 col-sm-6">
+                <div class="services">
+                    <img src="../../../assets/Services/accurate time.png" class="service-icon" alt="">
+                    <p>
+                        At our shopping site, we take great pride in our commitment to "On Time Delivery" - a promise
+                        that sets us apart from the rest. We understand that receiving your purchases promptly is
+                        crucial, and that's why we leave no stone unturned to ensure your orders reach you when you
+                        expect them.
+                    </p>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 col-sm-6">
+                <div class="services">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../../../assets/Services/easy replacement.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Easy Replacement</h5>
+                            <a data-bs-toggle="collapse" href="#replacement" role="button" aria-expanded="false"
+                                aria-controls="replacement">
+                                More.....
+                            </a>
+                            <div class="collapse" id="replacement">
+                                <p class="card-text">
+                                    At our shopping site, we believe in making your shopping experience as effortless as
+                                    possible,
+                                    which is why we take great pride in offering an "Easy Replacement" policy. We
+                                    understand that
+                                    sometimes, despite the best efforts, a product may not meet your expectations.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 col-sm-6">
+                <div class="services">
+                    <img src="../../../assets/Services/no contact delivery.png" class="service-icon" alt="">
+                    <p>
+                        At our shopping site, we understand the importance of safety and convenience, which is why we
+                        proudly offer "No Contact Delivery" for all our valued customers. With this service, we ensure
+                        that your orders are delivered right to your doorstep without the need for any physical contact.
+                    </p>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 col-sm-6">
+                <div class="services">
+                    <img src="../../../assets/Services/security.png" class="service-icon" alt="">
+                    <p>
+                        Ensuring the security of your purchases is a top priority at our shopping site. We understand
+                        the
+                        importance of safeguarding your valuable products, and we take every measure to provide the
+                        highest level of "Product Security." From the moment you place your order to its delivery at
+                        your doorstep, we implement robust security protocols to protect your items from any potential
+                        risks.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
--- app/src/navbar-sec/navbar.php
+++ app/src/navbar-sec/navbar.php
@@ -1,17 +1,16 @@
-<html lang="en">
 <?php include "../../../dbms/config.php";
 		session_start();
 	?>
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>JP MART</title>
     <link rel="icon" type="image/png" href="../favicon.ico">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="./navbar.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <style>
     #name {
         border-radius: 20px;
@@ -36,7 +35,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="homeNav nav-link active" aria-current="page"><svg
+                            <a class="navbarLinks nav-link active" aria-current="page"><svg
                                     xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                     class="bi bi-house-door-fill" viewBox="0 0 16 16">
                                     <path
@@ -44,22 +43,22 @@
                                 </svg> Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-tools" viewBox="0 0 16 16">
+                            <a class="navbarLinks nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                    height="16" fill="currentColor" class="bi bi-tools" viewBox="0 0 16 16">
                                     <path
                                         d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0Zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708ZM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11Z" />
                                 </svg> Services</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
+                            <a class="navbarLinks nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                    height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
                                     <path
                                         d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                                 </svg> My Cart</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-hand-index-fill" viewBox="0 0 16 16">
+                            <a class="navbarLinks nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                    height="16" fill="currentColor" class="bi bi-hand-index-fill" viewBox="0 0 16 16">
                                     <path
                                         d="M8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v5.34l-1.2.24a1.5 1.5 0 0 0-1.196 1.636l.345 3.106a2.5 2.5 0 0 0 .405 1.11l1.433 2.15A1.5 1.5 0 0 0 6.035 16h6.385a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002z" />
                                 </svg> Inquire</a>
@@ -70,7 +69,7 @@
                         <button class="btn btn-outline-success" type="submit">Search</button>
                         <?php
                                     if (isset($_SESSION['user'])) {
-                                    ?> <span id="dummy" style="display: none;"> <?php echo $_SESSION['email'] ?>
+                                        ?> <span id="dummy" style="display: none;"> <?php echo $_SESSION['email'] ?>
                         </span>
                         <a href="../login sys/logout.php">
                             <p class="text-dark my-1 px-3 text-center bg-warning p-1" id="name"></p>
@@ -84,11 +83,12 @@
                         </script> <?php
                                     } else {
                                     ?>
-                        <button class="btn mx-1 btn-log"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                fill="currentColor" class="bi bi-door-open-fill" viewBox="0 0 16 16">
-                                <path
-                                    d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
-                            </svg> Log In</button><?php
+                        <a class="navbarLinks btn-log"><button class="btn btn-outline-warning mx-1 btn-log"><svg
+                                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-door-open-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
+                                </svg> Log In</button></a><?php
                                     }
                                     ?>
 
@@ -96,98 +96,21 @@
                 </div>
             </nav>
         </div>
-    </div>
-    <!-- <div class="container-fluid">
-        <nav class="nav-section navbar navbar-expand-lg fixed-top">
-            <a class="navbar-brand" href="#">
-                
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-                aria-controls="offcanvasNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class=" offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
-                        <img src="../../../assets/logo.png" style="width: 60px;height: 30px;">
-                    </h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body bg-secondary">
-                    <form class="d-flex ms-2" role="search">
-                        <input class="form-control me-2 w-125 h-100" type="search" placeholder="Search"
-                            aria-label="Search">
-                        <button class="btn btn-outline-warning" type="submit">Search</button>
-                    </form>
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="../index/mainsection.php">
-                                <i class="fa-solid fa-house"></i> Home </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../menu/services.php">
-                                <i class="fa-solid fa-bars-progress"></i> Services </a>
-                        </li>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../menu/team.php">
-                                <i class="fa-solid fa-briefcase"></i> Our Team </a>
-                        </li>
-                        </li>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../menu/about.php">
-                                <i class="fa-solid fa-address-card"></i> About </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../menu/contactus.php">
-                                <i class="fa-solid fa-id-badge"></i> Contact </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../shoping_system/My_cart.php">
-                                <i class="fa-solid fa-cart-shopping"></i> Cart </a>
-                        </li> <?php
-                                    if (isset($_SESSION['user'])) {
-                                    ?> <span id="dummy" style="display: none;"> <?php echo $_SESSION['email'] ?>
-                        </span>
-                        <a href="../login sys/logout.php">
-                            <p class="text-dark my-1 px-3 text-center bg-warning p-1" id="name"></p>
-                        </a>
-                        <script>
-                        let txt = document.getElementById('name');
-                        let name = document.getElementById('dummy').innerHTML;
-                        let Sub_name = name.toUpperCase();
-                        let split_name = Sub_name.split('');
-                        txt.innerHTML = split_name[0];
-                        </script> <?php
-                                    } else {
-                                    ?> <li class="nav-item">
-                            <button type="button" class="btn btn-light float-end" data-bs-toggle="modal"
-                                data-bs-target="#staticBackdrop"> Login/SignUp </button>
-                        </li> <?php
-                                    }
-                                    ?>
-                    </ul>
-                </div>
+        <div class="nav2">
+            <div class="nav" id="nav">
+                <a href="#" class="nav-link" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop"
+                    aria-controls="offcanvasTop">All <i class="fa-solid fa-angles-right" id="all"></i>
+                </a>
+                <a href="../attaches/categories.php?Categorie=Mobile" class="nav-link">Mobile</a>
+                <a href="../attaches/categories.php?Categorie=Laptop" class="nav-link">Laptop</a>
+                <a href="../attaches/categories.php?Categorie=T.V" class="nav-link">T.V.</a>
+                <a href="../attaches/categories.php?Categorie=Refrigerator" class="nav-link">Refrigerator</a>
+                <a href="#" class="nav-link" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop"
+                    aria-controls="offcanvasTop">More.. </i>
+                </a>
             </div>
-        </nav>
-    </div> -->
-    <div class="nav2">
-        <div class="nav" id="nav">
-            <a href="#" class="nav-link" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop"
-                aria-controls="offcanvasTop">All <i class="fa-solid fa-angles-right" id="all"></i>
-            </a>
-            <a href="../attaches/categories.php?Categorie=Mobile" class="nav-link">Mobile</a>
-            <a href="../attaches/categories.php?Categorie=Laptop" class="nav-link">Laptop</a>
-            <a href="../attaches/categories.php?Categorie=T.V" class="nav-link">T.V.</a>
-            <a href="../attaches/categories.php?Categorie=Refrigerator" class="nav-link">Refrigerator</a>
-            <a href="#" class="nav-link" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop"
-                aria-controls="offcanvasTop">More.. </i>
-            </a>
         </div>
-    </div>
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        <!-- <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
         aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content bg-secondary">
@@ -227,47 +150,45 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasTopLabel">
-                <img src="../assets/logo.png" style="width: 60px;height: 30px;">
-            </h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-            <div class="row"> <?php
+    </div> -->
+        <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasTopLabel">
+                    <img src="../../../assets/logo.png" style="width:60px;height:30px;">
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <div class="row"> <?php
                     $sql = "SELECT * FROM `categorie`";
                     $result = mysqli_query($obj->connection, $sql);
-
+                    
                     while ($row = mysqli_fetch_row($result)) {
-                    ?> <div class="col-3 my-1 ">
-                    <a href="http://localhost/jp-tech/attaches/categories.php?Categorie=<?php echo $row[1] ?>"
-                        style="text-decoration: none;" class="text-dark border-bottom border-dark">
-                        <?php echo $row[1] ?> </a>
-                </div> <?php
+                        ?> <div class="col-3 my-1 ">
+                        <a href="http://localhost/jp-tech/attaches/categories.php?Categorie=<?php echo $row[1] ?>"
+                            style="text-decoration: none;" class="offcanvas-links">
+                            <?php echo $row[1] ?> </a>
+                    </div> <?php
                     }
                     ?> </div>
+            </div>
         </div>
     </div>
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
-    <script src="https://cdn.jsdelivr.net/i]npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
     </script>
     <script type="module">
     import {
         navbarSection
     } from "../../../routePath.js";
-    let homeNav = document.querySelector(".homeNav");
-    console.log(homeNav);
-    // console.log(navbarSection);
-    </script>
-    <script>
-    AOS.init();
+    let linksRef = document.querySelectorAll(".navbarLinks");
+    linksRef[0].href = navbarSection[0].url;
+    linksRef[1].href = navbarSection[1].url;
+    linksRef[2].href = navbarSection[2].url;
+    linksRef[3].href = navbarSection[3].url;
+    linksRef[4].href = navbarSection[4].url;
+    console.log(linksRef);
+    console.log(navbarSection);
     </script>
 </body>
 
